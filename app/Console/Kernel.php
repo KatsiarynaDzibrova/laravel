45bb7fc7b88dc8811dd7fabcfc37268a6b4f6e6c
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-02-22 12:24:36 +0300

    L-2: create random artist cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 
 use App\Console\Commands\RetrieveRecordings;
+use App\Console\Commands\RetrieveRandomArtist;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -16,7 +17,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        RetrieveRecordings::class
+        RetrieveRecordings::class,
+        RetrieveRandomArtist::class
     ];
 
     /**
@@ -28,6 +30,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('recordings:daily')->daily();
+        $schedule->command('artists:random')->hourly();
     }
 
     /**
