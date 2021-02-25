b5241dbf6372e2ab8040d74580b25651dbd37edc
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-02-25 23:25:57 +0300

    L-3: change model and settings to use jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,34 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
     protected $fillable = ['username',  'email', 'password', 'last_login_at', 'email_verified_at'];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+         return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
 }
