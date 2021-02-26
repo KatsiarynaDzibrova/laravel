d487b4c4df796c0da7b9bed3e5e3516e56a89a09
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-02-26 10:04:35 +0300

    L-3: reset password endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -66,6 +66,11 @@ class AuthController extends Controller
         return $this->respondWithToken(auth()->refresh());
     }
 
+    /**
+     * Register new user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register()
     {
         if (User::where('email', request('email'))->exists())
@@ -80,6 +85,22 @@ class AuthController extends Controller
         ]);
 
         return $this->login();
+    }
+
+    /**
+     * Reset users password.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reset() {
+        if (!User::where('email', request('email'))->exists())
+        {
+            return response()->json(['error' => 'no user with such email'], 404);
+        }
+
+        // TODO: send notification to email
+
+        return response()->json(['message' => 'Successfully logged out']);
     }
 
     /**
