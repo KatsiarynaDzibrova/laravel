e9c1d14a2929d2911bacae1494493bc295d94eee
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-03-03 21:59:49 +0300

    L-3: add authentification

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div>
+        <div>
+            <div class="card">
+                <h4>{{ __('Login') }}</h4>
+
+                <div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div>
+                            <label for="email">{{ __('E-Mail Address') }}</label>
+
+                            <div>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div >
+                            <label for="password" >{{ __('Password') }}</label>
+
+                            <div>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
