a40167fa8ca844eef5bfc8511203f4b4eab45f92
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2021-03-03 19:18:29 +0300

    Revert "L-3 restrict recording request"
    This reverts commit 20e29f4386cbfb58ca26bc925d20c01b7be72e57.

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,15 +15,6 @@ class User extends Authenticatable implements JWTSubject, CanResetPassword, Must
     protected $fillable = ['username',  'email', 'password', 'last_login_at', 'email_verified_at'];
 
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
      * @return mixed
